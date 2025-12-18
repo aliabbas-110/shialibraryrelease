7579388f2f0dbc43ffbbb3a45731272f259301d7
aliabbas-110 <aliabbasnr110@gmail.com>
2025-12-18 15:03:48 +1100

    indexing fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      staticDir: 'dist',
-      routes: [
-        '/',
-        '/about',
-        '/contact',
-      ]
-    })
-  ]
+  plugins: [react()],
 })
+
+
