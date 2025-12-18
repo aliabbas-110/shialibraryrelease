fbdf56acf6ed7bdbb0d3a780203a551296070abf
aliabbas-110 <aliabbasnr110@gmail.com>
2025-12-18 14:58:57 +1100

    indexing2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      staticDir: 'dist',
+      routes: [
+        '/',
+        '/about',
+        '/contact',
+      ]
+    })
+  ]
 })
